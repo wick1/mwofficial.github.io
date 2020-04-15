981e7eeded891e63d780ef4efdbd6b2d3b456838
wick1 <38076660+wick1@users.noreply.github.com>
2020-04-15 13:33:09 +0500

    Changes

--- mail.php
+++ mail.php
@@ -1,19 +1,37 @@
 <?php
 
-    $to = "mr.mw.96@gmail.com.com"; 
-    $from = $_REQUEST['email']; 
-    $name = $_REQUEST['name'];
-    $headers = "From: $from"; 
-    $subject = "You have a message sent from your site"; 
+    // $to = "mr.mw.96@gmail.com.com"; 
+    // $from = $_REQUEST['email']; 
+    // $name = $_REQUEST['name'];
+    // $headers = "From: $from"; 
+    // $subject = "You have a message sent from your site"; 
 
-    $fields = array(); 
-    $fields{"name"} = "name"; 
-    $fields{"email"} = "email"; 
-    $fields{"subject"} = "subject"; 
-    $fields{"message"} = "message";
+    // $fields = array(); 
+    // $fields{"name"} = "name"; 
+    // $fields{"email"} = "email"; 
+    // $fields{"subject"} = "subject"; 
+    // $fields{"message"} = "message";
 
-    $body = "Here is what was sent:\n\n"; foreach($fields as $a => $b){   $body .= sprintf("%20s: %s\n",$b,$_REQUEST[$a]); }
+    // $body = "Here is what was sent:\n\n"; foreach($fields as $a => $b){   $body .= sprintf("%20s: %s\n",$b,$_REQUEST[$a]); }
 
-    $send = mail($to, $subject, $body, $headers);
+    // $send = mail($to, $subject, $body, $headers);
+
+
+if(isset($_POST)) {
+  $to = "mr.mw.96@gmail.com.com"; // your mail here
+  $name = filter_var($_POST["name"], FILTER_SANITIZE_EMAIL);
+  $email = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
+  $subject = filter_var($_POST["subject"], FILTER_SANITIZE_STRING);
+  $message = filter_var($_POST["message"], FILTER_SANITIZE_STRING);
+  $body = "Message: $message\nName: $name\nE-mail: $email";
+  
+  if(@mail($to, $subject, $body)) {
+    $output = json_encode(array('success' => true));
+    die($output);
+  } else {
+    $output = json_encode(array('success' => false));
+    die($output);
+  }
+}
 
 ?>
